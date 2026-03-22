a858b2ead29f72e98aaa30186afb8ee5dd8a5ff2
anames2323 <137616037+anames2323@users.noreply.github.com>
2026-03-22 17:16:58 +1000

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,35 @@
+TOKEN = "8757937028:AAGT2AwXYgj2ND7B5Nf4a5Kc0-o6fcNDJMw" #сюда токен бота
+
+admins_id = [
+    8795006636
+]
+
+SUBGRAM_TOKEN = "b7dae10fecc75a7a9c1031c5a2a61d8d86f0156abf21647399867adb4205ceb3" # токен subgram
+HIVIEWS_TOKEN = "UA6TXBT1RD0ZRAGRHRY5R"
+
+
+admin_username = "@arseniylap2011" #свой username
+admin_link = "https://t.me/arseniylap2011"
+
+GIFT_AMOUNT = 1.0 # ежедневый гифт
+DAILY_COOLDOWN = 24 * 60 * 60 # не трогать
+DELAY_TIME = 90 # не трогать
+
+#основной канал
+channel_osn = "https://t.me/skillstarschannel"
+#чат
+chater = "https://t.me/chatskillstars"
+id_chat = -1003734418290
+#канал выплат
+channel_viplat = "https://t.me/viplatyskillstars"
+channel_viplat_id = -1003870668105
+#канал выигрышей мини-игры:
+channel_link = "https://t.me/gamesskillstars"
+id_channel_game = -1003532605221
+
+#Обязательная подписка
+required_subscription = []
+
+#кнопка в старте по запросу
+beta_url = ""
+beta_name = ""
